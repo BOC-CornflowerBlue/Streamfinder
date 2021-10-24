1383070bdd854df605d0c242ca565fd2e26e512b
Mark P Thomas <markums@gmail.com>
2021-10-24 00:24:10 -0600

    Added clickTracker component and demo file

---
+++ docs/componentDemos/clickTrackerDemo.js
@@ -0,0 +1,16 @@
+// import ClickTracker from '../trackers/POClickTracker';
+
+// // Merely place the target element within the tags.
+// // Any events within child elements bubble up and are caught at this level to be reported.
+
+// return (
+//   <div id="po-cart" class="column">
+//     <ClickTracker eventName="clickTracker" moduleName="Product Overview">
+//       <AddButton
+//         label={ 'add to bag' }
+//         id={ 'checkout' }
+//         onClick={ this.handleCheckout }
+//       />
+//     </ClickTracker>
+//   </div>
+// );
