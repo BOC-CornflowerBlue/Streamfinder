1156a9312a589304793910d34b8c84bacf331fe4
Mark P Thomas <markums@gmail.com>
2021-10-25 09:40:28 -0600

    Forgot to save config ;-P

--- server/config/config.js
+++ server/config/config.js
@@ -9,4 +9,29 @@ module.exports = {
   redis_host: env.REDIS_HOST,
   redis_port: env.REDIS_PORT,
   redis_password: env.REDIS_PASSWORD,
+
+  development: {
+    url: env.DEV_DATABASE_URL_MONGO,
+    database: env.DEV_DATABASE_MONGO,
+    host: env.HOST_MONGO,
+    port: env.PORT_MONGO,
+    username: env.USERNAME_MONGO,
+    password: env.PASSWORD_MONGO,
+  },
+  test: {
+    url: env.TEST_DATABASE_URL_MONGO,
+    database: env.TEST_DATABASE_MONGO,
+    host: env.HOST_MONGO,
+    port: env.PORT_MONGO,
+    username: env.USERNAME_MONGO,
+    password: env.PASSWORD_MONGO,
+  },
+  production: {
+    url: env.DATABASE_URL_MONGO,
+    database: env.DATABASE_MONGO,
+    host: env.HOST_MONGO,
+    port: env.PORT_MONGO,
+    username: env.USERNAME_MONGO,
+    password: env.PASSWORD_MONGO,
+  },
 }
