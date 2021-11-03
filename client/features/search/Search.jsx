d12b23bb83fb306bc3bdb7cde3eef52b66dd88d3
Steven Harder <stevenharderjr@gmail.com>
2021-11-02 19:04:46 -0600

    Aesthetic improvements

--- client/features/search/Search.jsx
+++ client/features/search/Search.jsx
@@ -1,63 +1,62 @@
 import React from 'react';
 import SearchBar from '../sharedComponents/SearchBar';
 import activeMessage from '../sharedComponents/helpers/activeMessage';
+import TMDB_image from '../TMDB/TMDB_image';
 import './Search.css';
-import axios from 'axios';
-import { ironMan } from './tempData';
-import TempDisplay1 from './TempDisplay1';
-import TempDisplay2 from './TempDisplay2';
-//remove once carosel can be installed/replace in Search class
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      searchVal: '',
-      searchDisplay: []
-    };
-    this.handleClick = this.handleClick.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
-  }
-  handleSearch(e) {
-    //sets up searchVal state to be sent to server for processing
-    let searchValue = e.target.value;
-    this.setState({
-      searchVal: searchValue
-    });
+    this.activePlaceholder = this.activePlaceholder.bind(this);
+    this.updatePlaceholder = this.updatePlaceholder.bind(this);
+    this.revertPlaceholder = this.revertPlaceholder.bind(this);
+
+    this.placeholder = 'Search content by title';
+    this.cancelActiveMessage = undefined;
+
+    this.state = {
+      searchResults: [],
+      placeholder: this.placeholder,
+      activeMessage: undefined
+    }
   }
 
   handleClick(e) {
-    //sends search value state/updates state
-    //do stuff with server
-    //set state with results
-    this.setState({
-      searchDisplay: ironMan
-    });
+
   }
+
+  handleKeyPress(e) {
+    e.key === 'Enter' && this.handleSubmit(e);
+  }
+
+  handleSearch(searchTerm) {
+    this.cancelActiveMessage = activeMessage(`Searching for content entitled "${searchTerm}"`, this.updatePlaceholder);
+    // setTimeout(this.revertPlaceholder, 2000);
+  }
+
+  revertPlaceholder() {
+    this.cancelActiveMessage();
+    this.setState({ placeholder: this.placeholder });
+  }
+
+  updatePlaceholder(message) {
+    this.setState({ placeholder: message || this.placeholder });
+  }
+
+  activePlaceholder(message) {
+    setInterval(this.updatePlaceholder, 200);
+  }
+
   render() {
     const { searchTerm, placeholder } = this.state;
     const { handleSearch } = this;
 
     return (
-      <div>
-        <div id="Search">
-          <h1 className='search-header'>Stream Finder</h1>
-          <input
-            className='search-box'
-            type="text"
-            placeholder='search a movie to display streaming providers 🎣'
-            value={this.state.searchVal}
-            onChange={this.handleSearch}
-          />
-          <button
-            onClick={this.handleClick}
-            className='search-button'>Search
-          </button>
-        </div>
-        <TempDisplay1 data={this.state.searchDisplay}/>
-        <TempDisplay2 data={this.state.searchDisplay}/>
-
+      <div id="Search">
+        <h1 className="home-title">Streamfinder</h1>
+        <SearchBar placeholder={ placeholder } handleSearch={ handleSearch } />
       </div>
     );
   }
