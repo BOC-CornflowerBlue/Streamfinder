206d1ad6457e4394da076d19a9a17517052c239d
Chris Monteilh <chris@sunsationalswimschool.com>
2021-11-12 00:26:42 -0800

    Push to git to transfer machines

--- client/features/reviews/ReviewCard.jsx
+++ client/features/reviews/ReviewCard.jsx
@@ -5,10 +5,16 @@ import './../media/MediaDetail.css';
 import StarRating from '../sharedComponents/StarRating.jsx';
 
 const ReviewCard = review => {
-  const { username, rating, content } = review;
+  const { username, rating, content, thisUsersReview } = review;
+
+  let divClass = 'review-card';
+
+  if (thisUsersReview) {
+    divClass = divClass + ' this-users-review';
+  }
 
   return (
-    <div className='review-card'>
+    <div className={divClass} >
       <div className='review-card-header'>
         <div className='username'>{username}</div>
         <div className='userRating'><StarRating avgRating={rating} /></div>
