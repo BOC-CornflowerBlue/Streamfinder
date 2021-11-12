51869937c31ef7b02b093a3f5bbf05c836b8940e
Chris Monteilh <chris@sunsationalswimschool.com>
2021-11-11 22:28:00 -0800

    Use StarRating.jsx to display star ratings for each user review

--- client/features/reviews/ReviewCard.jsx
+++ client/features/reviews/ReviewCard.jsx
@@ -2,21 +2,16 @@ import React from 'react';
 import StarRatingInteractive from '../sharedComponents/StarRatingInteractive';
 import Reviews from '../reviews/Reviews.jsx';
 import './../media/MediaDetail.css';
+import StarRating from '../sharedComponents/StarRating.jsx';
 
 const ReviewCard = review => {
-  const { username, rating, content, thisUsersReview } = review;
-
-  let divClass = `'review-card'`;
-
-  if (thisUsersReview) {
-    divClass = `'this-users-review-card`;
-  }
+  const { username, rating, content } = review;
 
   return (
-    <div className={divClass} >
+    <div className='review-card'>
       <div className='review-card-header'>
         <div className='username'>{username}</div>
-        <div className='userRating'>{rating}</div>
+        <div className='userRating'><StarRating avgRating={rating} /></div>
       </div>
       <div className='review-text'>
         {content}
